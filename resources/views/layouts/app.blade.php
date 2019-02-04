723fbde799d22738d98280ee66e9402d3937c736
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-02-05 01:40:05 +0545

    Frontend setup to be continued

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,102 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <title>TheSaaS - Header Image</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{asset('assets/css/core.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/thesaas.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="{{asset('assets/img/apple-touch-icon.png')}}">
+    <link rel="icon" href="{{asset('assets/img/favicon.png')}}">
+
+
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+<div id="app">
 
-                    </ul>
+</div>
+<!-- Topbar -->
+<nav class="topbar topbar-inverse topbar-expand-md topbar-sticky">
+    <div class="container">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+        <div class="topbar-left">
+            <button class="topbar-toggler">&#9776;</button>
+            <a class="topbar-brand" href="index.html">
+                <img class="logo-default" src="{{asset('assets/img/logo.png')}}" alt="logo">
+                <img class="logo-inverse" src="{{asset('assets/img/logo-light.png')}}" alt="logo">
+            </a>
+        </div>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <div class="topbar-right">
+            <ul class="topbar-nav nav">
+                <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-link active" href="javascript:">Login</a></li>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+            </ul>
+        </div>
+
     </div>
+</nav>
+<!-- END Topbar -->
+
+
+
+
+<!-- Header -->
+@yield('header')
+<!-- END Header -->
+
+
+
+
+<!-- Main container -->
+<main class="main-content">
+
+
+    @yield('content')
+
+
+
+</main>
+<!-- END Main container -->
+
+
+
+
+
+
+<!-- Footer -->
+<footer class="site-footer">
+    <div class="container">
+        <div class="row gap-y justify-content-center">
+            <div class="col-12 col-lg-6">
+                <ul class="nav nav-primary nav-hero">
+                    <li class="nav-item hidden-sm-down">
+                        <a class="nav-link" href="/">Bahdcasts</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- END Footer -->
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('assets/js/core.min.js') }}"></script>
+<script src="{{ asset('assets/js/thesaas.min.js') }}"></script>
+<script src="{{ asset('assets/js/script.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
